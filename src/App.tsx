3badf6bf0f37db2f46b55f2a377ce0fdec2ad752
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-03-18 19:09:00 -0300

    Modified components with Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
-import ProductDetails from "./routes/ProductDetails";
+import Catalog from "./routes/Catalog";
 
 export default function App() {
-  return <ProductDetails />;
+  return <Catalog />;
 }
