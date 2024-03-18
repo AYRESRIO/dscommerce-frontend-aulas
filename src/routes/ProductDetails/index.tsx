5c95296c5a049a1e2a59bf4a4b192d665bf1b1cf
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-03-18 16:11:05 -0300

    Props-Categories

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -1,44 +1,46 @@
-import './styles.css';
+import "./styles.css";
 import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
-import { ProductDTO } from '../../models/product';
+import { ProductDTO } from "../../models/product";
 
 const product: ProductDTO = {
   id: 2,
   name: "Smart TV",
   description: "Esta TV é muito bonita",
-  imgUrl:"https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
+  imgUrl:
+    "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
   price: 2500.99,
   categories: [
     {
       id: 2,
-      name: "Eletrônicos"
+      name: "Eletrônicos",
     },
     {
       id: 3,
-      name: "Computadores"
-    }
-  ]
-
-}
+      name: "Computadores",
+    },
+    {
+      id: 4,
+      name: "Importados",
+    },
+  ],
+};
 
 export default function ProductDetails() {
-
-    return(
-     <>
-    <HeaderClient/>
-    <main>
-      <section id="product-details-section" className="dsc-container">
-        <ProductDetailsCard product ={product} />
-        <div className="dsc-btn-page-container">
-            < ButtonPrimary/>
-            <ButtonInverse/>
-        </div>
-      </section>
-    </main>
-  </>
-
-    );
+  return (
+    <>
+      <HeaderClient />
+      <main>
+        <section id="product-details-section" className="dsc-container">
+          <ProductDetailsCard product={product} />
+          <div className="dsc-btn-page-container">
+            <ButtonPrimary />
+            <ButtonInverse />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
