eed50730c5ab658ca980d0b1956e66e1b94dde4e
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2025-01-22 18:25:52 -0300

    Modifications.

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import Catalog from "./routes/ClientHome/Catalog";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ClientHome />}>
-          <Route index element={<Catalog />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route
-            path="product-details/:productId"
-            element={<ProductDetails />}
-          />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+         <Route path="/" element={<ClientHome />}>
+           <Route index element={<Catalog />} />
+           <Route path="catalog" element={<Catalog />} />
+           <Route
+             path="product-details/:productId"
+             element={<ProductDetails />}
+           />
+         </Route>
+         <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
   );
 }
