887dcf45efc4b390bf7048441378c17cf60fe0ad
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2025-02-04 10:48:08 -0300

    created page Cart and route cart

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,25 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import ClientHome from "./routes/ClientHome";
 import Catalog from "./routes/ClientHome/Catalog";
+import Cart from "./routes/ClientHome/Cart";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-         <Route path="/" element={<ClientHome />}>
-           <Route index element={<Catalog />} />
-           <Route path="catalog" element={<Catalog />} />
-           <Route
-             path="product-details/:productId"
-             element={<ProductDetails />}
-           />
-         </Route>
-         <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/" element={<ClientHome />}>
+            <Route index element={<Catalog />} />
+            <Route path="catalog" element={<Catalog />} />
+            <Route
+              path="product-details/:productId"
+              element={<ProductDetails />}
+            />
+            <Route path="cart" element={<Cart />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
-
     </>
   );
 }
