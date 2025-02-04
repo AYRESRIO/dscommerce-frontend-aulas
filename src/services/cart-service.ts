320da119137e9b68cb8942f23072cb6244351658
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2025-02-04 17:40:12 -0300

    cart save/get

--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -4,3 +4,7 @@ import * as cartRepository from "../localstorage/cart-repository"
 export function saveCart(Cart: OrderDTO) {
   cartRepository.save(Cart);
 }
+
+export function getCart() : OrderDTO {
+    return cartRepository.get();
+}
