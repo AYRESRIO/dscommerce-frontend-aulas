87ef267ee1da9f7adb783d31fd357c41a46b4e0d
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2025-02-25 21:37:43 -0300

    Admin page layout

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -3,6 +3,7 @@ import { CredentialsDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
+import * as accessTokenRepository from "../localstorage/acces-token-repository";
 
 export function loginRequest(loginData: CredentialsDTO) {
   const headers = {
@@ -10,13 +11,27 @@ export function loginRequest(loginData: CredentialsDTO) {
     Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET),
   };
 
-  const requestBody = QueryString.stringify({...loginData, grant_type: "password" }); 
-  const config:  AxiosRequestConfig = {
+  const requestBody = QueryString.stringify({
+    ...loginData,
+    grant_type: "password",
+  });
+  const config: AxiosRequestConfig = {
     method: "POST",
     url: "/oauth/token",
     data: requestBody,
-    headers
-  }
+    headers,
+  };
   return requestBackend(config);
 }
 
+export function logout() {
+  accessTokenRepository.remove();
+}
+export function saveAccessToken(token: string) {
+  accessTokenRepository.save(token);
+}
+
+export function getAcessToken() {
+  accessTokenRepository.get();
+
+}
