09e2fee64074b9973e7c334ee3db2100799b3396
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2025-03-06 17:18:26 -0300

    Forms updateAll

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -16,3 +16,15 @@ export function toValues(inputs: any) {
   );
 
 }
+
+export function updateAll(inputs: any, newValues: any) {
+
+  const newInputs: any = {};
+  for ( const name in inputs) {
+      newInputs[name] = { ...inputs[name], value: newValues[name] };
+  }
+
+  return (
+    newInputs
+  );
+}
